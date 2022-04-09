0ea058fd4bba2c571c3923dbd2a1cdc6bab20996
orhunpusat <orhunpusat3000@gmail.com>
2022-04-09 03:20:27 +0300

    work 3 is done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,9 @@ public class HelloWorld {
 
     // doing some work here ( you can type anything you want to)
 
-    //doing avesome work 2.
+    //doing awesome work 2.
+
+    //doing awesome work 3
+    //awesome workis done and ready to be commited
+    //and ready to be push
 }
